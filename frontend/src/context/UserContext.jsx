e85fdb4614bb7d60c9552cf63ab977d3d24f1073
Ayush Gupta <ayushgupta1925@gmail.com>
2025-09-26 20:56:18 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useEffect, useState } from "react";
 export const userDataContext = createContext();
 
 function UserContext({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://virtualassistant-backend-n623.onrender.com";
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(false);
 const [selectedImage,setSelectedImage] = useState(null)
